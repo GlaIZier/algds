b8cee6c1c0bd1a17dd82ebe90aae5c013e894a73
Mikhail Khokhlushin <mkhokhlushin@smile-net.ru>
2016-12-22 19:17:50 +0300

    StringSort interface has been added

---
+++ src/main/java/ru/glaizier/alg/sort/string/Lsd.java
@@ -0,0 +1,10 @@
+package ru.glaizier.alg.sort.string;
+
+public class Lsd implements StringSort {
+
+    @Override
+    public void sort(String[] a) {
+
+    }
+
+}
---
+++ src/main/java/ru/glaizier/alg/sort/string/StringSort.java
@@ -0,0 +1,8 @@
+package ru.glaizier.alg.sort.string;
+
+@FunctionalInterface
+public interface StringSort {
+
+    void sort(String[] a);
+
+}

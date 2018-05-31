0c13a08b05d88373750303acb89938949be72b16
GlaIZier <kh.mv@bk.ru>
2018-05-31 12:10:06 +0300

    lazy list: LazyList has been update to support previous

--- src/main/java/ru/glaizier/algds/ds/lazylist/LazyList.java
+++ src/main/java/ru/glaizier/algds/ds/lazylist/LazyList.java
@@ -5,6 +5,7 @@ import java.util.function.UnaryOperator;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 /**
  * @author GlaIZier
@@ -13,7 +14,10 @@ import lombok.RequiredArgsConstructor;
 public class LazyList<T> {
     @Getter
     private final T value;
-    private final Supplier<LazyList<T>> next;
+    @Getter
+    private final LazyList<T> prev;
+    @Setter
+    private Supplier<LazyList<T>> next;
 
     public LazyList<T> next() {
         return next.get();
@@ -24,7 +28,15 @@ public class LazyList<T> {
         private final UnaryOperator<T> transformer;
 
         public LazyList<T> from(T seed) {
-            return new LazyList<>(seed, () -> from(transformer.apply(seed)));
+            return from(seed, null);
+        }
+
+        private LazyList<T> from(T seed, LazyList<T> prev) {
+            LazyList<T> created = new LazyList<>(seed, prev);
+            // To be able to link this to the previous node with created, we need to avoid catch-22 problem.
+            // So, we need a setter for the next function. Otherwise, we cannot point at created list as the prev one.
+            created.setNext(() -> from(transformer.apply(seed), created));
+            return created;
         }
     }
 
@@ -33,5 +45,6 @@ public class LazyList<T> {
 
         System.out.println(factory.from(0).getValue());
         System.out.println(factory.from(0).next().next().getValue());
+        System.out.println(factory.from(0).next().next().getPrev().getValue());
     }
 }

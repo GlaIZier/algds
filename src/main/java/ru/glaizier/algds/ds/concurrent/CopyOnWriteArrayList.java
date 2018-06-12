df32e0d0a322815d1af2011d78a0f8e16b8f1f68
GlaIZier <kh.mv@bk.ru>
2018-06-12 12:21:49 +0300

    CopyOnWriteArrayList has been done

--- src/main/java/ru/glaizier/algds/ds/concurrent/CopyOnWriteArrayList.java
+++ src/main/java/ru/glaizier/algds/ds/concurrent/CopyOnWriteArrayList.java
@@ -1,11 +1,8 @@
 package ru.glaizier.algds.ds.concurrent;
 
 import java.util.ArrayList;
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Function;
 
 public class CopyOnWriteArrayList<T> {
 
@@ -13,17 +10,51 @@ public class CopyOnWriteArrayList<T> {
 
     private final AtomicLong version = new AtomicLong();
 
-    public void add(T t) {
+    public void add(T elem) {
+        compareAndSwapArray(array -> {
+            array.add(elem);
+            return null;
+        });
+    }
+
+    public void add(int index, T elem) {
+        compareAndSwapArray(array -> {
+            array.add(index, elem);
+            return null;
+        });
+    }
+
+    public T set(int index, T elem) {
+        return compareAndSwapArray(array -> array.set(index, elem));
+    }
+
+    public T remove(int index) {
+        return compareAndSwapArray(array -> array.remove(index));
+    }
+
+    public boolean remove(T elem) {
+        return compareAndSwapArray(array -> array.remove(elem));
+    }
+
+    public T get(int index) {
+        return array.get(index);
+    }
+
+    public int size() {
+        return array.size();
+    }
+
+    private <R> R compareAndSwapArray(Function<? super ArrayList<T>, ? extends R> arrayTransformer) {
         while (true) {
             long prevVersion = this.version.get();
             long newVersion = prevVersion++;
 
-            ArrayList<T> newArray = new ArrayList<>();
-            newArray.addAll(array);
-            newArray.add(t);
+            ArrayList<T> newArray = new ArrayList<>(array);
+            R result = arrayTransformer.apply(newArray);
+
             if (version.compareAndSet(prevVersion, newVersion)) {
                 array = newArray;
-                return;
+                return result;
             }
         }
     }

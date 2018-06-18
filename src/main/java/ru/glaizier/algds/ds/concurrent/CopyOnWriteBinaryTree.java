c4be614b85b831881727c14bbcd1bb6db7925327
GlaIZier <kh.mv@bk.ru>
2018-06-18 12:06:12 +0300

    copyonwritebinarytree: CopyOnWriteBinaryTree has been updated

--- src/main/java/ru/glaizier/algds/ds/concurrent/CopyOnWriteBinaryTree.java
+++ src/main/java/ru/glaizier/algds/ds/concurrent/CopyOnWriteBinaryTree.java
@@ -1,4 +1,13 @@
 package ru.glaizier.algds.ds.concurrent;
 
-public class CopyOnWriteBinaryTree {
+public class CopyOnWriteBinaryTree<T> {
+
+    private static class Node<T> {
+        private final Node<T> left;
+        private final Node<T> right;
+        public Node(Node<T> left, Node<T> right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
 }

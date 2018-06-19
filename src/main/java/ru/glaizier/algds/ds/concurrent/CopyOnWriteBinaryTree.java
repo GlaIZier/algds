20716a2385b38220d42619e8f1ff08a088852d18
GlaIZier <kh.mv@bk.ru>
2018-06-19 13:09:08 +0300

    copyonwritebinarytree: CopyOnWriteBinaryTree has been updated

--- src/main/java/ru/glaizier/algds/ds/concurrent/CopyOnWriteBinaryTree.java
+++ src/main/java/ru/glaizier/algds/ds/concurrent/CopyOnWriteBinaryTree.java
@@ -1,13 +1,24 @@
 package ru.glaizier.algds.ds.concurrent;
 
-public class CopyOnWriteBinaryTree<T> {
+import java.util.concurrent.atomic.AtomicLong;
 
-    private static class Node<T> {
-        private final Node<T> left;
-        private final Node<T> right;
-        public Node(Node<T> left, Node<T> right) {
-            this.left = left;
-            this.right = right;
-        }
+import lombok.Value;
+
+public class CopyOnWriteBinaryTree<T extends Comparable> {
+
+    private volatile Node<T> root;
+
+    private final AtomicLong version = new AtomicLong();
+
+    @Value
+    private static class Node<T extends Comparable> {
+        Node<T> left;
+        Node<T> right;
+        T value;
     }
+
+    public void add(T value) {
+
+    }
+
 }

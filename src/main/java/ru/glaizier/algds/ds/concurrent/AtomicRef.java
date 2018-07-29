55de9bba30ce93873c27bdd1644a0e0a98c29980
GlaIZier <kh.mv@bk.ru>
2018-07-29 18:44:02 +0300

    AtomicRef: AtomicRef has been updated

--- src/main/java/ru/glaizier/algds/ds/concurrent/AtomicRef.java
+++ src/main/java/ru/glaizier/algds/ds/concurrent/AtomicRef.java
@@ -2,26 +2,43 @@ package ru.glaizier.algds.ds.concurrent;
 
 import sun.misc.Unsafe;
 
+import java.lang.reflect.Field;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class AtomicRef<V> {
+    private static final Unsafe unsafe = getUnsafe();
 
-    private static final Unsafe unsafe = Unsafe.getUnsafe();
     private static final long valueOffset;
+
+    private volatile V value;
+
+    private static Unsafe getUnsafe() {
+        try {
+            Field f = Unsafe.class.getDeclaredField("theUnsafe");
+            f.setAccessible(true);
+            return (Unsafe) f.get(null);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     static {
         try {
             valueOffset = unsafe.objectFieldOffset
-                    (AtomicReference.class.getDeclaredField("value"));
-        } catch (Exception ex) { throw new Error(ex); }
+                    (AtomicRef.class.getDeclaredField("value"));
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
-
-    private volatile V value;
 
     public AtomicRef(V initialValue) {
         value = initialValue;
     }
 
-    public AtomicRef() {
+    public AtomicRef() {}
+
+    public static void main(String[] args) {
+        System.out.println("valueOffset = " + valueOffset);
     }
+
 }
